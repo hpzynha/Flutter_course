ec00be14cf97694d5da8e7b0879d8b52f93c5c42
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-03-11 20:06:11 +1030

    4 day

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -22,7 +22,24 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
-        context: context, builder: (ctx) => const NewExpense());
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) => NewExpense(
+        onAddExpense: _addExpense,
+      ),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
   }
 
   @override
@@ -45,6 +62,7 @@ class _ExpensesState extends State<Expenses> {
           Expanded(
             child: ExpensesList(
               expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
             ),
           ),
         ],
--- expense_tracker/lib/widgets/expenses_list/expense_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expense_list.dart
@@ -1,16 +1,26 @@
 import 'package:expense_tracker/models/expenses.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: expenses.length,
-        itemBuilder: (ctx, index) => ExpenseItem(expenses[index]));
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => onRemoveExpense(expenses[index]),
+        child: ExpenseItem(
+          expenses[index],
+        ),
+      ),
+    );
   }
 }
